85df11b65dc711ce566c35e3f2245f4ae6fd4aa1
Derrick Bray <derrickbray@yahoo.com>
2016-10-11 14:50:06 -0500

    for loop

--- app/index.js
+++ app/index.js
@@ -1,1 +1,51 @@
 import 'whatwg-fetch';
+const crew = [
+  'Bender',
+  'Fry',
+  'Leela',
+  'Amy',
+  'Professor',
+  'Zoidburg',
+  'Hermes',
+  'Nibbler',
+  'Scruffy',
+];
+
+const competition = [
+  'Bender',
+  'Fry',
+  'Leela',
+  'Amy',
+  'Professor',
+  'Zoidburg',
+  'Hermes',
+  'Nibbler',
+  'Scruffy',
+];
+
+console.log(crew[0]);
+console.log(crew[1]);
+console.log(crew[2]);
+console.log(crew[3]);
+console.log(crew[4]);
+console.log(crew[5]);
+console.log(crew[6]);
+console.log(crew[7]);
+console.log(crew[8]);
+console.log(crew.length);
+console.log(crew[crew.length - 1]);
+
+let counter = 100;
+
+while (counter < 102) {
+  console.log('Kill all humans');
+
+  counter = counter + 1;
+}
+for (let x = 1; x < 5; = x += 1) {
+  console.log('shut up and take my money');
+}
+
+for (let i = 0; i < crew.length = i++){
+
+}
