ebcfd49f8718548d1fe09327c32e6d0ba20d5102
Derrick Bray <derrickbray@yahoo.com>
2016-10-11 14:17:52 -0500

    js

---
+++ app/public/hoisting.js
@@ -0,0 +1,18 @@
+import 'whatwg-fetch';
+
+console.log(x);
+console.log(y);
+console.log(a);
+
+let a = 'This is different';
+console.log(d);
+const d = 25;
+
+
+if (true) {
+  var x = 2;
+}
+
+if (false) {
+  var y = 3;
+}
---
+++ public/hoisting.js
@@ -0,0 +1,24 @@
+'use strict';
+
+console.log(y);
+console.log(a);
+
+let a = 'This is different';
+console.log(d);
+const d = 25;
+
+
+if (true) {
+  var x = 2;
+  let foo = 'bar';
+}
+console.log(x);
+
+console.log(v);
+function lunch(v) {
+  var inside = 1;
+}
+
+if (false) {
+  var y = 3;
+}
